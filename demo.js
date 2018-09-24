251d1ec39e856a7127326360daf719e9d1413f46
ninabreznik <ninabreznik@gmail.com>
2018-09-24 22:50:01 +0100

    Remove width and height from constructor

--- demo.js
+++ demo.js
@@ -2,9 +2,12 @@ var logo = require('./')
 
 var opts = {
   urls: ['', 'https://i.imgur.com/Q4qAH30.jpg', 'https://i.imgur.com/sZK75ef.png'],  // if you have URL and color on the same position, URL overwrites it
-  colors: ['red', 'blue', ''],
-  width: '500px',
-  height: '500px'
+  colors: ['red', 'blue', '']
 }
 
-document.body.appendChild(logo(opts))
+var el = document.createElement('div')
+el.style.width = '100px'
+el.style.height = '100px'
+el.appendChild(logo(opts))
+
+document.body.appendChild(el)
--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ module.exports = logo
 function logo (opts) {
   var colors = opts.colors
   var urls = opts.urls
-  var width = opts.width
-  var height = opts.height
 
   var scale = 3
   var scaleX = 1 * scale
@@ -55,7 +53,7 @@ function logo (opts) {
 
 var el = document.createElement('div')
 el.innerHTML = `
-  <svg width="${width}" height="${height}" viewBox="0 -20 600 700">
+  <svg viewBox="0 0 600 800">
     <defs>
       <pattern id="img0" patternUnits="objectBoundingBox" width="1" height="1">
         <image xlink:href="${urls[0]}" x="0" y="0"/>
